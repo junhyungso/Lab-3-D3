bdac1cf551e15f2a591772741daf72c6f6d90512
Junhyung So <jun.so@ww.com>
2020-09-30 11:09:47 -0400

    final changes

--- d3.js
+++ d3.js
@@ -1,7 +1,8 @@
 d3.csv('cities.csv', d3.autoType).then(data=>{
 	let filteredData = data.filter(e=>e.eu===true);
+	console.log(filteredData)
 	d3.select('.city-count')
-		.text(filteredData.length + ' European Cities');
+		.text('Number of European Cities: ' + filteredData.length);
 	const width = 700;
 	const height = 550;
 	const svg = d3.select('.population-plot')
@@ -13,10 +14,8 @@ d3.csv('cities.csv', d3.autoType).then(data=>{
 	.data(filteredData)
 	.enter()
 	.append('circle')
-	.attr('cx', (d, i) => {
-		return (i * 23 + 20);
-	})
-	.attr('cy', 30)
+	.attr('cx', (d, i) => d.x)
+	.attr('cy', (d,i) => d.y)
 	.attr('r', d => d.population < 1000000? 4 : 8)
 	.attr("fill", "green")
 
@@ -25,8 +24,8 @@ d3.csv('cities.csv', d3.autoType).then(data=>{
 	.data(filteredData)
 	.enter()
 	.append("text")
-	.attr('dx', (d,i) => (i*23 + 20))
-	.attr('dy', 50)
+	.attr('dx', (d,i) => d.x)
+	.attr('dy', (d,i) => d.y - 10)
 	.filter((d,i) => d.population >= 1000000)
 	.text(d=>d.country)	
 	.attr("font-size", "11px")
@@ -37,8 +36,7 @@ d3.csv('cities.csv', d3.autoType).then(data=>{
 
 d3.csv('buildings.csv', d3.autoType).then(data=>{
 	let sortedData = data.sort((a,b) => b.height_ft - a.height_ft);
-	d3.select('.building-height')
-		.text('World Building Heights')
+	console.log(sortedData)
 	const width = 500;
 	const height = 500;
 	const svg = d3.select('.bar-buildings')
@@ -57,7 +55,7 @@ d3.csv('buildings.csv', d3.autoType).then(data=>{
 		.attr('x', (d,i) => 175)
 		.attr('y', (d,i) => (i * width/sortedData.length) + 20)
 		.attr('width', (d,i) => d.height_px)
-		.attr('height', 20)
+		.attr('height', 25)
 		.attr('class', 'bar-details')
 		.on("click", (d, i) => {
 			d3.select('.image').attr('src', "img/" + i.image)
@@ -65,6 +63,8 @@ d3.csv('buildings.csv', d3.autoType).then(data=>{
 			d3.select('.height').text(i.height_ft)
 			d3.select('.city').text(i.city)
 			d3.select('.country').text(i.country)
+			d3.select('.floors').text(i.floors)
+			d3.select('.completed').text(i.completed)
 
 			svg.selectAll('table.height')
 				.data(sortedData)
@@ -73,6 +73,7 @@ d3.csv('buildings.csv', d3.autoType).then(data=>{
 			d3.select("td#height").append('span').text(i.height_ft)
 			d3.select("td#city").text((i) => i.city)
 			d3.select("td#country").text((i) => i.country)
+			d3.select("td#completed").text((i) => i.completed)
 		  });
 						
 	svg.selectAll("text.labels")
@@ -80,7 +81,7 @@ d3.csv('buildings.csv', d3.autoType).then(data=>{
 		.enter()
 		.append("text")
 		.attr('dx', 0)
-		.attr('dy', (d,i) => (i * width/sortedData.length) + 32 )
+		.attr('dy', (d,i) => (i * width/sortedData.length) + 37 )
 		.text(d=>d.building)	
 		.attr("font-size", "11px")
 		.attr("text-anchor", "front")
@@ -92,7 +93,7 @@ d3.csv('buildings.csv', d3.autoType).then(data=>{
 		.append("text")
 		.attr('dx', (d,i) => (d.height_px) + 170)
 		.attr('dy', (d,i) => (i * width/sortedData.length) + 35 )
-		.text(d=> (d.height_m + 'm'))	
+		.text(d=> (d.height_ft + ' ft'))	
 		.attr("font-size", "11px")
 		.attr("text-anchor", "end")
 		.attr("fill", "white")
